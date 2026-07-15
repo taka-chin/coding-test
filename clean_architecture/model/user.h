fdd18d67ea9ad25b0c04de67f7194f880f42427d
takachin <ty80164@gmail.com>
2026-07-15 18:59:28 +0900

    name.hの作成・単体テスト追加

--- clean_architecture/model/user.h
+++ clean_architecture/model/user.h
@@ -5,13 +5,12 @@ using namespace std;
 
 // ユーザーを表すクラス
 class User {
-  private:
-    int id;
-    string name;
-    
-  public:
-    User() : id(0), name(""){}
-    
-    User(int id, const string& name)
-        id(id), name(name){}
-}
+ private:
+  int id;
+  string name;
+
+ public:
+  User() : id(0), name("") {}
+
+  User(int id, const string& name) : id(id), name(name) {}
+};
---
+++ clean_architecture/tests/domain/name_test.cpp
@@ -0,0 +1,14 @@
+#include "../../model/name.h"
+
+#include <gtest/gtest.h>
+
+TEST(NameTest, ValidName) {
+  Name name("Taka");
+  EXPECT_EQ(name.getValue(), "Taka");
+}
+
+TEST(NameTest, EmptyName) { EXPECT_THROW(Name(""), std::invalid_argument); }
+
+TEST(NameTest, DigitsOnly) {
+  EXPECT_THROW(Name("12345"), std::invalid_argument);
+}
