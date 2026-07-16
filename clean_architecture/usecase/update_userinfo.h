4bec86032b746e4e31ce459ea0d46171597fa8e4
takachin <ty80164@gmail.com>
2026-07-17 06:12:06 +0900

    add usecase

---
+++ clean_architecture/usecase/update_userinfo.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "../domain/user.h"
+#include "../domain/name.h"
+#include "../domain/email.h"
+#include "../domain/phone_number.h"
+#include "user_repository.h"
+#include "notification_service.h"
+
+// ユーザー更新処理
+// 1. user_id と更新内容を受け取る
+// 2. UserRepository から User を取得する
+// 3. ドメインのルールで更新できるか確認する
+// 4. Name / Email / PhoneNumber を使って値を作る
+// 5. User を更新する
+// 6. UserRepository に保存する
+// 7. 必要なら NotificationService に通知を依頼する
+
+class UpdateUserInfo {
+private:
+    UserRepository* userRepository;
+    NotificationService* notificationService;
+
+public:
+    UpdateUserInfoUseCase(
+        UserRepository* userRepository,
+        NotificationService* notificationService
+    ) : userRepository(userRepository),
+        notificationService(notificationService) {}
+    
+    UserInfo execute(const std::string& userId, const UpdateUserRequest& request) {
+        // ここにユースケースの手順を書く
+    }
+};
+
